87f7609f47c190758d0a5bf06bbb673ee14fa62d
Артем Ермак <artyom.rus.ct100@mail.ru>
2021-03-30 20:42:14 +0300

    init:test-api

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            const string GETUSER_URL = "https://dummyapi.io/data/api/user?limit=10";
+            const string appId = "60630884ce28c8d3a5d836e3";
+            using var getUsers = new HttpClient();
+            getUsers.DefaultRequestHeaders.Add("app-id", appId);
+            var result = await getUsers.GetStringAsync(GETUSER_URL);
+            dynamic json = JsonConvert.DeserializeObject(result);
+            var data = json.data;
+            List<User> users = new List<User>();
+            foreach (var values in data)
+            {
+                User user = new User(values.id.ToString(), values.lastName.ToString(), values.firstName.ToString(), values.email.ToString());
+                users.Add(user);
+            }
+            string output = JsonConvert.SerializeObject(users, Formatting.Indented);
+            return output;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi.Models
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        public User(string id, string lastName, string firstName, string email)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
+
